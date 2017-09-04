a18f347870a0c79ff6b761747c38226bad0c3998
Dmitriy Ermakov <neil.ermak@gmail.com>
2017-09-04 23:34:33 +0300

    Added base,index templates for films
    Added static files (CSS, JS, Background
    
    Split URL patterns for films and glogal
    
    Added admin CRUD for genre and films

--- films/admin.py
+++ films/admin.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Film, Genre
 
-# Register your models here.
+admin.site.register(Film)
+admin.site.register(Genre)
--- films/views.py
+++ films/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Film, Genre
 
-# Create your views here.
+
+def index(request):
+    films = Film.objects.all()
+    return render(request, 'films/index.html')
